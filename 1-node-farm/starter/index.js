c93a203cdb3ef3f4975cd21998f2d1d19cb6713a
Clinton Odia <odiamehiclinton@gmail.com>
2025-08-23 07:30:48 +0100

    Update: fs read and write using async

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -4,11 +4,42 @@ const fs = require("fs");
 
 // console.log(hello);
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// Blocking, Synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
-// console.log(textIn);
+// // console.log(textIn);
 
-const textOut = `This is what we know about avocado: ${textIn}. \n`;
-fs.writeFileSync("./txt/output.txt", textOut);
+// const textOut = `This is what we know about avocado: ${textIn}. \n`;
+// fs.writeFileSync("./txt/output.txt", textOut);
 
-console.log("file has been written");
+// console.log("file has been written");
+
+// Non-blocking, Asynchronous way
+// simple version with one callback
+// fs.readFile("./txt/start.txt","utf-8", 
+//   (err, data) => {console.log(data);}
+// );
+// console.log("reading file");
+
+// complex version with multiple callbacks
+
+fs.readFile("./txt/start.txt","utf-8", 
+  //first callback
+  (err, data1) => {console.log(data1);
+    fs.readFile(`./txt/${data1}.txt`,"utf-8", 
+      //second callback
+  (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`,"utf-8", 
+      //third callback
+    (err, data3) => {
+      console.log(data3);
+      fs.writeFile("./txt/final.txt", `${data2} \n ${data3}`,"utf-8", (err) => {
+        console.log("writing file");
+      })
+    })})
+  }
+
+
+);
+console.log("reading file");
